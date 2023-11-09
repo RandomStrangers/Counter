fc40d505812a7b11d61cdc92cd3a35cf62c42424
RandomStrangers <28567960-RandomStrangers@users.noreply.replit.com>
2023-11-09 12:22:19 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,44 @@
+using System;
+
+class Counter
+{
+    public static int x;
+    public static int y;
+    public const string InternalVersion = "1.0.7";
+    public static string Name = "RandomStrangers";
+    [STAThread]
+
+        static void Main(string[] args)
+        {
+        args = null;
+        Console.Title = "Counter" + " v" + InternalVersion + " By " + Name;
+
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.BackgroundColor = ConsoleColor.Black;
+        while (x != int.MaxValue || y != int.MinValue)
+        {
+            x++;
+            y--;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            
+        }
+
+        if (x == int.MaxValue)
+        {
+            Console.WriteLine("Max limit reached!");
+            x = 0;
+            Console.WriteLine("Press any key to restart!");
+            Console.ReadKey(true);
+            Main(null);
+        }
+        if (y == int.MinValue)
+        {
+            Console.WriteLine("Min limit reached!");
+            y = 0;
+            Console.WriteLine("Press any key to restart!");
+            Console.ReadKey(true);
+            Main(null);
+        }
+    }
+          }

00f070c2784795a1d7f7e2ecad20e075d858b5d4
Random Strangers <110739593+RandomStrangers@users.noreply.github.com>
2024-10-10 17:52:31 -0400

    Add files via upload

---
+++ 1.1.6A/0.0-1.0.0/Counter 0.0.cs
@@ -0,0 +1,13 @@
+﻿public class Counter_0_0
+{
+    public static System.Byte x;
+    public const System.String Version = "0.0.0.0";
+    public static void Main()
+    {
+        while (Counter_0_0.x != System.Byte.MaxValue)
+        {
+            Counter_0_0.x++;
+            System.Console.WriteLine("System.Byte:" + Counter_0_0.x);
+        }
+    }
+}
---
+++ 1.1.6A/0.0-1.0.0/Counter 0.7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Counter_0_7
+{
+    public static int x;
+    public const string Version = "0.7";
+    public static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.BackgroundColor = ConsoleColor.Black;
+        while (x != int.MaxValue)
+        {
+            x++;
+            Console.WriteLine(x);
+        }
+        if (x == int.MaxValue)
+        {
+            Console.WriteLine("Max limit reached!");
+            x = 0;
+            Console.WriteLine("Press any key to restart!");
+            Console.ReadKey(true);
+            Main();
+        }
+    }
+}

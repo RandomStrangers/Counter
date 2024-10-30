5a04bd442263524b345c79f109fbc57d48a865e6
RandomStrangers <jackstage527@gmail.com>
2024-10-29 23:47:20 -0400

    Update 1.1.6 (continued)
    Fix v1.1.4 sbyte/byte sorting.

--- 1.1.4/Counter.cs
+++ 1.1.4/Counter.cs
@@ -14,148 +14,174 @@ public class Counter
     public static short j = short.MaxValue;
     public static ushort k = ushort.MinValue;
     public static ushort l = ushort.MaxValue;
-    public static byte m = byte.MinValue;
-    public static byte n = byte.MaxValue;
-    public static sbyte o = sbyte.MinValue;
-    public static sbyte p = sbyte.MaxValue;
+    public static sbyte m = sbyte.MinValue;
+    public static sbyte n = sbyte.MaxValue;
+    public static byte o = byte.MinValue;
+    public static byte p = byte.MaxValue;
     public const string InternalVersion = "1.1.4";
     public static void Main(string[] args)
     {
-      Console.Title = "Counter" + " v" + InternalVersion;
-      Console.ForegroundColor = ConsoleColor.DarkRed;
-      Console.BackgroundColor = ConsoleColor.Black;
-        while (a != int.MaxValue || b != int.MinValue || c != uint.MaxValue || d != uint.MinValue || e != long.MaxValue || f != long.MinValue || g != ulong.MaxValue || h != ulong.MinValue || i != short.MaxValue || j != short.MinValue || k != ushort.MaxValue || l != ushort.MinValue || m != byte.MaxValue || n != byte.MinValue || o != sbyte.MaxValue || p != sbyte.MinValue)
+        Console.Title = "Counter v" + InternalVersion;
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.BackgroundColor = ConsoleColor.Black;
+        while (a != int.MaxValue || b != int.MinValue
+            || c != uint.MaxValue || d != uint.MinValue
+            || e != long.MaxValue || f != long.MinValue
+            || g != ulong.MaxValue || h != ulong.MinValue
+            || i != short.MaxValue || j != short.MinValue
+            || k != ushort.MaxValue || l != ushort.MinValue
+            || m != sbyte.MaxValue || n != sbyte.MinValue
+            || o != byte.MaxValue || p != byte.MinValue)
         {
-          if (a != int.MaxValue) 
-          {
-            a++;
-            Console.WriteLine(a);
-          }
-          if (a == int.MaxValue) {
-            Console.WriteLine("A is " + int.MaxValue);
-          }
-          if (b != int.MinValue) 
-          {
-            b--;
-            Console.WriteLine(b);
-          }
-          if (b == int.MinValue) {
-            Console.WriteLine("B is " + int.MinValue);
-          }
-          if (c != uint.MaxValue) 
-          {
-            c++;
-            Console.WriteLine(c);
-          }
-          if (c == uint.MaxValue) {
-            Console.WriteLine("C is " + uint.MaxValue);
-          }
-          if (d != uint.MinValue) 
-          {
-            d--;
-            Console.WriteLine(d);
-          }
-          if (d == uint.MinValue) {
-            Console.WriteLine("D is " + uint.MinValue);
-          }
-          if (e != long.MaxValue) 
-          {
-            e++;
-            Console.WriteLine(e);
-          }
-          if (e == long.MaxValue) {
-            Console.WriteLine("E is " + long.MaxValue);
-          }
-          if (f != long.MinValue) 
-          {
-            f--;
-            Console.WriteLine(f);
-          }
-          if (f == long.MinValue) {
-            Console.WriteLine("F is " + long.MinValue);
-          }
-          if (g != ulong.MaxValue) 
-          {
-            g++;
-            Console.WriteLine(g);
-          }
-          if (g == ulong.MaxValue) {
-            Console.WriteLine("G is " + ulong.MaxValue);
-          }
-          if (h != ulong.MinValue) 
-          {
-            h--;
-            Console.WriteLine(h);
-          }
-          if (h == ulong.MinValue) {
-            Console.WriteLine("H is " + ulong.MinValue);
-          }
-          if (i != short.MaxValue) 
-          {
-            i++;
-            Console.WriteLine(i);
-          }
-          if (i == short.MaxValue) {
-            Console.WriteLine("I is " + short.MaxValue);
-          }
-          if (j != short.MinValue) 
-          {
-            j--;
-            Console.WriteLine(j);
-          }
-          if (j == short.MinValue) {
-            Console.WriteLine("J is " + short.MinValue);
-          }
-          if (k != ushort.MaxValue) 
-          {
-            k++;
-            Console.WriteLine(k);
-          }
-          if (k == ushort.MaxValue) {
-            Console.WriteLine("K is " + ushort.MaxValue);
-          }
-          if (l != ushort.MinValue) 
-          {
-            l--;
-            Console.WriteLine(l);
-          }
-          if (l == ushort.MinValue) {
-            Console.WriteLine("L is " + ushort.MinValue);
-          }
-          if (m != byte.MaxValue) 
-          {
-            m++;
-            Console.WriteLine(m);
-          }
-          if (m == byte.MaxValue) {
-            Console.WriteLine("M is " + byte.MaxValue);
-          }
-          if (n != byte.MinValue) 
-          {
-            n--;
-            Console.WriteLine(n);
-          }
-          if (n == byte.MinValue) {
-            Console.WriteLine("N is " + byte.MinValue);
-          }
-          if (o != sbyte.MaxValue) 
-          {
-            o++;
-            Console.WriteLine(o);
-          }
-          if (o == sbyte.MaxValue) {
-            Console.WriteLine("O is " + sbyte.MaxValue);
-          }
-          if (p != sbyte.MinValue) 
-          {
-            p--;
-            Console.WriteLine(p);
-          }
-          if (p == sbyte.MinValue) {
-            Console.WriteLine("P is " + sbyte.MinValue);
-          }
+            if (a != int.MaxValue)
+            {
+                a++;
+                Console.WriteLine(a);
+            }
+            if (a == int.MaxValue)
+            {
+                Console.WriteLine("A is " + int.MaxValue);
+            }
+            if (b != int.MinValue)
+            {
+                b--;
+                Console.WriteLine(b);
+            }
+            if (b == int.MinValue)
+            {
+                Console.WriteLine("B is " + int.MinValue);
+            }
+            if (c != uint.MaxValue)
+            {
+                c++;
+                Console.WriteLine(c);
+            }
+            if (c == uint.MaxValue)
+            {
+                Console.WriteLine("C is " + uint.MaxValue);
+            }
+            if (d != uint.MinValue)
+            {
+                d--;
+                Console.WriteLine(d);
+            }
+            if (d == uint.MinValue)
+            {
+                Console.WriteLine("D is " + uint.MinValue);
+            }
+            if (e != long.MaxValue)
+            {
+                e++;
+                Console.WriteLine(e);
+            }
+            if (e == long.MaxValue)
+            {
+                Console.WriteLine("E is " + long.MaxValue);
+            }
+            if (f != long.MinValue)
+            {
+                f--;
+                Console.WriteLine(f);
+            }
+            if (f == long.MinValue)
+            {
+                Console.WriteLine("F is " + long.MinValue);
+            }
+            if (g != ulong.MaxValue)
+            {
+                g++;
+                Console.WriteLine(g);
+            }
+            if (g == ulong.MaxValue)
+            {
+                Console.WriteLine("G is " + ulong.MaxValue);
+            }
+            if (h != ulong.MinValue)
+            {
+                h--;
+                Console.WriteLine(h);
+            }
+            if (h == ulong.MinValue)
+            {
+                Console.WriteLine("H is " + ulong.MinValue);
+            }
+            if (i != short.MaxValue)
+            {
+                i++;
+                Console.WriteLine(i);
+            }
+            if (i == short.MaxValue)
+            {
+                Console.WriteLine("I is " + short.MaxValue);
+            }
+            if (j != short.MinValue)
+            {
+                j--;
+                Console.WriteLine(j);
+            }
+            if (j == short.MinValue)
+            {
+                Console.WriteLine("J is " + short.MinValue);
+            }
+            if (k != ushort.MaxValue)
+            {
+                k++;
+                Console.WriteLine(k);
+            }
+            if (k == ushort.MaxValue)
+            {
+                Console.WriteLine("K is " + ushort.MaxValue);
+            }
+            if (l != ushort.MinValue)
+            {
+                l--;
+                Console.WriteLine(l);
+            }
+            if (l == ushort.MinValue)
+            {
+                Console.WriteLine("L is " + ushort.MinValue);
+            }
+            if (m != sbyte.MaxValue)
+            {
+                m++;
+                Console.WriteLine(m);
+            }
+            if (m == sbyte.MaxValue)
+            {
+                Console.WriteLine("M is " + sbyte.MaxValue);
+            }
+            if (n != sbyte.MinValue)
+            {
+                n--;
+                Console.WriteLine(n);
+            }
+            if (n == sbyte.MinValue)
+            {
+                Console.WriteLine("N is " + sbyte.MinValue);
+            }
+            if (o != byte.MaxValue)
+            {
+                o++;
+                Console.WriteLine(o);
+            }
+            if (o == byte.MaxValue)
+            {
+                Console.WriteLine("O is " + byte.MaxValue);
+            }
+            if (p != byte.MinValue)
+            {
+                p--;
+                Console.WriteLine(p);
+            }
+            if (p == byte.MinValue)
+            {
+                Console.WriteLine("P is " + byte.MinValue);
+            }
         }
-        if (a == int.MaxValue && c == uint.MaxValue && e == long.MaxValue && g == ulong.MaxValue && i == short.MaxValue && k == ushort.MaxValue && m == byte.MaxValue && o == sbyte.MaxValue)
+        if (a == int.MaxValue && c == uint.MaxValue
+            && e == long.MaxValue && g == ulong.MaxValue
+            && i == short.MaxValue && k == ushort.MaxValue
+            && m == sbyte.MaxValue && o == byte.MaxValue)
         {
             Console.WriteLine("Max limit reached!");
             a = int.MinValue;
@@ -164,13 +190,16 @@ public class Counter
             g = ulong.MinValue;
             i = short.MinValue;
             k = ushort.MinValue;
-            m = byte.MinValue;
-            o = sbyte.MinValue;
+            m = sbyte.MinValue;
+            o = byte.MinValue;
             Console.WriteLine("Press any key to restart!");
             Console.ReadKey(true);
             Main(null);
         }
-        if (b == int.MinValue && d == uint.MinValue && f == long.MinValue && h == ulong.MinValue && j == short.MinValue && l == ushort.MinValue && n == byte.MinValue && p == sbyte.MinValue)
+        if (b == int.MinValue && d == uint.MinValue
+            && f == long.MinValue && h == ulong.MinValue
+            && j == short.MinValue && l == ushort.MinValue
+            && n == sbyte.MinValue && p == byte.MinValue)
         {
             Console.WriteLine("Min limit reached!");
             b = int.MaxValue;
@@ -179,8 +208,8 @@ public class Counter
             h = ulong.MaxValue;
             j = short.MaxValue;
             l = ushort.MaxValue;
-            n = byte.MaxValue;
-            p = sbyte.MaxValue;
+            n = sbyte.MaxValue;
+            p = byte.MaxValue;
             Console.WriteLine("Press any key to restart!");
             Console.ReadKey(true);
             Main(null);
